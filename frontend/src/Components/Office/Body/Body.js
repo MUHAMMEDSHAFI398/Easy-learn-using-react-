7ea99ce8465cfc8118872d4403ab6eb93a2b6b51
MUHAMMEDSHAFI398 <shafimuhammed398@gmail.com>
2023-01-31 09:29:52 +0530

    login page completely designes

--- frontend/src/Components/Office/Body/Body.js
+++ frontend/src/Components/Office/Body/Body.js
@@ -1,74 +1,48 @@
 import React from 'react'
-import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBCol, MDBRow, MDBInput } from 'mdb-react-ui-kit';
 import './Body.css'
 
 
 function Body() {
   return (
-   
+
     <div className='border-login container  mb-5 '  >
       <MDBContainer fluid className="p-3 my-5 h-custom">
-   
+
         <MDBRow>
 
           <MDBCol col='10' md='6'>
-            <img className='image' src="images/girl-1.png"  alt="" />
+            <img className='image' src="images/girl-1.png" alt="" />
           </MDBCol>
 
           <MDBCol col='4' md='6'>
-            
-          <div className='border-login' >
-          
-          <div className="d-flex flex-row align-items-center justify-content-center">
-              <h1 className="hi mb-5 mt-3">Login</h1>
-    
-            </div>
 
-            <MDBInput wrapperClass='mb-4 ms-4 me-4' label='Email address' id='formControlLg' type='email' size="lg" />
-            <MDBInput wrapperClass='mb-4 ms-4 me-4' label='Password' id='formControlLg' type='password' size="lg" />
+            <div className='border-login ' >
 
-            <div className='text-center text-md-start mt-4 pt-2'>
-              <div className='d-flex justify-content-center'>
-              <button type='submit' style={{width:'570px',height:'45px'}} className='btn btn-success mb-5'>Login</button>
+              <div className="d-flex flex-row align-items-center justify-content-center">
+                <h1 className="hi mb-5 mt-3">Login</h1>
+
               </div>
-            </div>
-          </div>
 
-            
+
+              <MDBInput wrapperClass='mb-4 ms-4 me-4' className='input' label='Email address' id='formControlLg' type='email' size="lg" />
+              <MDBInput wrapperClass='mb-4 ms-4 me-4' className='input' label='Password' id='formControlLg' type='password' size="lg" />
+              <MDBInput  className='submit-btn' wrapperClass='mb-4 ms-4 me-4' type='submit' value='Login' id='formControlLg' size="lg" />
+
+
+
+            </div>
+
+
 
           </MDBCol>
 
         </MDBRow>
 
-        <div className="">
+        
+      </MDBContainer>
+    </div>
 
-    
-
-          <div>
-
-            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-              <MDBIcon fab icon='facebook-f' size="md" />
-            </MDBBtn>
-
-            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-              <MDBIcon fab icon='twitter' size="md" />
-            </MDBBtn>
-
-            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-              <MDBIcon fab icon='google' size="md" />
-            </MDBBtn>
-
-            <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-              <MDBIcon fab icon='linkedin-in' size="md" />
-            </MDBBtn>
-
-          </div>
-
-        </div>
-
-      </MDBContainer> 
-      </div>
-       
   )
 }
 
